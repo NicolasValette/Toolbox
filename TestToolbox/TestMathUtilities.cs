70bed1416a6bf3da7a8a8a9a58628c8ce350836c
NikoMyoji <nicolas.vlte@gmail.com>
2024-01-26 15:56:01 +0100

    Initial commit

---
+++ TestToolbox/TestMathUtilities.cs
@@ -0,0 +1,69 @@
+﻿using Toolbox.Utilities;
+
+namespace TestToolbox
+{
+    public class TestMathUtilities
+    {
+        [Test]
+        public void PrimesTest()
+        {
+            List<long> expectedResultFor10 = new List<long> { 2, 3, 5, 7 };
+            
+            var resultFor10 = MathUtilities.Primes(10);
+            var resultNegative = MathUtilities.Primes(-10);
+            var resultZero = MathUtilities.Primes(0);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultFor10.Count, Is.EqualTo(expectedResultFor10.Count), $"Primes must return list of {expectedResultFor10.Count} elements instead of {resultFor10.Count}");
+                Assert.That(resultFor10[0], Is.EqualTo(expectedResultFor10[0]));
+                Assert.That(resultFor10[1], Is.EqualTo(expectedResultFor10[1]));
+                Assert.That(resultFor10[2], Is.EqualTo(expectedResultFor10[2]));
+                Assert.That(resultFor10[3], Is.EqualTo(expectedResultFor10[3]));
+            });
+
+            Assert.That(resultNegative.Count, Is.EqualTo(0));
+            
+            Assert.That(resultZero.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FactorTest()
+        {
+            // 37 is prime
+            var resultFor37 = MathUtilities.Factor(37);
+            // 35478 = 2 x 3^5 x 73
+            var resultFor35478 = MathUtilities.Factor(35478);
+            var resultForNeg = MathUtilities.Factor(-1);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultFor37.Count, Is.EqualTo(1));
+                Assert.That(resultFor37[0].Item1, Is.EqualTo(37));
+                Assert.That(resultFor37[0].Item2, Is.EqualTo(1));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultFor35478.Count, Is.EqualTo(3));
+                Assert.That(resultFor35478[0].Item1, Is.EqualTo(2));
+                Assert.That(resultFor35478[0].Item2, Is.EqualTo(1));
+                Assert.That(resultFor35478[1].Item1, Is.EqualTo(3));
+                Assert.That(resultFor35478[1].Item2, Is.EqualTo(5));
+                Assert.That(resultFor35478[2].Item1, Is.EqualTo(73));
+                Assert.That(resultFor35478[2].Item2, Is.EqualTo(1));
+            });
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultForNeg.Count, Is.EqualTo(0));
+            });
+        }
+
+        [Test]
+        public void LCMTest()
+        {
+
+        }
+    }
+
+
+}
---
+++ Toolbox/Utilities/MathUtilities.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Utilities
+{
+ 
+    /// <summary>
+    /// This class contains multiple mathematical function
+    /// </summary>
+    public class MathUtilities
+    {
+        /// <summary>
+        /// This method return the list of the prime number, from 1 to max
+        /// </summary>
+        /// <param name="max">We compute prime until this number</param>
+        /// <returns>The list of the prime number from 1 to max</returns>
+        public static List<long> Primes(long max)
+        {
+            List<long> primes = new List<long>();
+            for (int i = 2; i <= max; i++)
+                if (!primes.Any(p => i % p == 0))
+                    primes.Add(i);
+            return primes;
+        }
+
+        /// <summary>
+        /// Give Least Common Multiples of the given numbers
+        /// </summary>
+        /// <param name="numbers">List of numbers to compute LCM</param>
+        /// <returns>The list of LCM of given numbers</returns>
+        public static long LCM(List<long> numbers)
+        {
+            List<(long, long)> factors = new List<(long, long)>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<(long, long)> factor = Factor(numbers[i]);
+                for (int j = 0; j < factor.Count; j++)
+                {
+                    if (factors.Exists(x => x.Item1 == factor[j].Item1))
+                    {
+                        int ind = factors.FindIndex(x => x.Item1 == factor[j].Item1);
+                        if (factors[ind].Item2 < factor[j].Item2)
+                        {
+                            factors[ind] = factor[j];
+                        }
+                    }
+                    else
+                    {
+                        factors.Add(factor[j]);
+                    }
+                }
+
+            }
+            long ppcm = 1;
+            for (int i = 0; i < factors.Count; i++)
+            {
+                ppcm *= (long)System.Math.Pow(factors[i].Item1, factors[i].Item2); ;
+            }
+            return ppcm;
+        }
+
+        /// <summary>
+        /// Give the list of the prime factor of one number
+        /// </summary>
+        /// <param name="number">the number that we want to facorize</param>
+        /// <returns>The list of the prime factor of the given number</returns>
+        public static List<(long, long)> Factor(long number)
+        {
+            List<(long, long)> factor = new List<(long, long)>();
+            List<long> primes = Primes(number);
+            long actual = number;
+            for (int i = 0; i < primes.Count; i++)
+            {
+                if (actual % primes[i] == 0)
+                {
+                    int exp = 1;
+                    actual = actual / primes[i];
+                    while (actual % primes[i] == 0)
+                    {
+                        exp++;
+                        actual = actual / primes[i];
+                    }
+                    factor.Add((primes[i], exp));
+                }
+                if (actual <= 1)
+                {
+                    break;
+                }
+            }
+            return factor;
+        }
+    }
+}
