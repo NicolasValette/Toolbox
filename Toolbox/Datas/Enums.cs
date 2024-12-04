556cab4a4d1f19b215bc32e3fffa1bd329a813e5
NikoMyoji <nicolas.vlte@gmail.com>
2024-12-04 13:08:32 +0100

    Create Enums

---
+++ Toolbox/Datas/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Datas
+{
+    public enum Directions
+    {
+        SOUTH,
+        NORTH,
+        EAST,
+        WEST,
+        SOUTHEAST,
+        SOUTHWEST,
+        NORTHEAST,
+        NORTHWEST
+    }
+}
